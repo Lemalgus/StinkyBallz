b1e58996e5527424d094cfac7e4f82bb286eabc5
Mooseley03 <166773350+Mooseley03@users.noreply.github.com>
2024-12-06 00:19:41 -0600

    added statement for negative values with Branches

--- retrieveReg.c
+++ retrieveReg.c
@@ -1,27 +1,24 @@
+
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+int global_i = 1;
+	
+void print_binary(int num) {
+    int i;
+    for (i = 10; i >= 0; i--) { // Assuming 32-bit integer
+        int bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+}
 
 
-void main(){
-
-bool rType = false;
-bool iType = false;
-bool dType = true;
-bool bType = false;
-bool cbType = false;
-bool iwType = false;
-
-int instruction = 0b11111000000001000000001000100001;
-char buffer[40];
-
-
-
-int i = 1;
-
-if (rType) {
+void r_type(int instruction, int opcode) {
+	char buffer[40];
     int Rd = instruction & 0x1F;
     instruction = instruction >> 5;
     int Rn = instruction & 0x1F;
@@ -60,7 +57,8 @@ if (rType) {
     }
 }
 
-else if (iType) {
+void i_type(int instruction, int opcode) {
+	char buffer[40];
     int Rd = instruction & 0x1F;
     instruction = instruction >> 5;
     int Rn = instruction & 0x1F;
@@ -86,7 +84,8 @@ else if (iType) {
     printf("%s\n", RdString);
 }
 
-else if (dType) {
+void d_type(int instruction, int opcode) {
+	char buffer[40];
     int Rt = instruction & 0x1F;
     instruction = instruction >> 5;
     int Rn = instruction & 0x1F;
@@ -120,10 +119,15 @@ else if (dType) {
 
 }
 
-else if (bType) {
+void b_type(int instruction, int opcode) {
+	char buffer[40];
     int BRAdd = instruction & 0x3FFFFFF;
-    BRAdd += i;
-    i++;
+
+    if ((BRAdd >> 26) & 1) {
+        BRAdd = ~BRAdd + 1;
+    }
+
+    BRAdd += global_i;
 
 
     char BRString[40] = "X";
@@ -133,12 +137,20 @@ else if (bType) {
     printf("%s\n L", BRString);
 }
 
-else if (cbType) {
+void cb_type(int instruction, int opcode) {
+	char buffer[40];
     int Rt = instruction & 0x1F; //value that determines ge, lt, etc
     instruction = instruction >> 5;
     int COND_BR_ADD = instruction & 0x8FFFF;
-    COND_BR_ADD += i;
-    i++;
+
+    if ((COND_BR_ADD >> 18) & 1) {
+        COND_BR_ADD = ~COND_BR_ADD + 1;
+    }
+    
+		//handle if negative or psitive 
+		//(twos to binary) -> (binary to decimal)
+		
+    COND_BR_ADD += global_i;
     char ext[5];
 
     if (Rt == 0) {
@@ -186,7 +198,8 @@ else if (cbType) {
     printf("%s\n", RtString);
 }
 
-else if (iwType) {
+void iw_type(int instruction, int opcode) {
+	char buffer[40];
     int Rd = instruction & 0x1F;
     instruction = instruction >> 5;
     int Mov_Imm = instruction & 0xFFFF;
@@ -207,5 +220,193 @@ else if (iwType) {
 
     printf("%s\n", RdString);
 }
+
+
+int main() {
+    int instruction = 0b11111000000001000000001000100001;
+	char buffer[40];
+	
+	bool rType = false;
+	bool iType = false;
+	bool dType = true;
+	bool bType = false;
+	bool cbType = false;
+	bool iwType = false;
+
+	
+	
+	//extract opcode
+	int opcode = (bitstring >> 21) & 0x7ff;
+	
+	printf("Binary representation of %d: ", opcode);
+    print_binary(opcode);
+	
+	
+	//B (is only 6 bits, so we need to include the not B.COND because B.COND has similar start as B
+	if(((opcode >> 5) & 0x05) == (opcode >> 5)){
+		printf("B ");
+		b_type(instruction, opcode);
+		
+		
+	}//B.COND
+	else if(((opcode >> 3) & 0x54) == (opcode >> 3)){
+		printf("B.");
+		cb_type(instruction, opcode);
+
+	
+	}//AND
+	else if((opcode & 0x450) == opcode){
+		printf("AND ");
+		r_type(instruction, opcode);
+	
+	
+	}//ADD command
+	else if((opcode & 0x458) == opcode){
+		printf("ADD ");
+		r_type(instruction, opcode);
+		
+		
+	}//ADDI
+	else if(((opcode >> 1) & 0x244) == (opcode >> 1)){
+		printf("ADDI ");
+		i_type(instruction, opcode);
+		
+	
+	}//ANDI
+	else if(((opcode >> 1) & 0x248) == (opcode >> 1)){
+		printf("ANDI ");
+		i_type(instruction, opcode);
+	
+
+	}//BL  
+	else if(((opcode >> 5) & 0x25) == (opcode >> 5)){
+		printf("BL ");
+		b_type(instruction, opcode);
+		
+		
+	}//MUL
+	else if((opcode & 0x4D8) == opcode){
+		printf("MUL ");
+		r_type(instruction, opcode);
+		
+		
+	}//ORR
+	else if((opcode & 0x150) == opcode){
+		printf("ORR ");
+		r_type(instruction, opcode);
+		
+		
+	}//ORRI
+	else if(((opcode >> 1) & 0x2C8) == (opcode >> 1)){
+		printf("ORRI ");
+		i_type(instruction, opcode);
+		
+		
+	}//CBZ
+	else if(((opcode >> 3) & 0xB4) == (opcode >> 3)){
+		printf("CBZ ");
+		cb_type(instruction, opcode);
+	
+	
+	}//CBNZ
+	else if(((opcode >> 3) & 0xB5) == (opcode >> 3)){
+		printf("CBNZ ");
+		cb_type(instruction, opcode);
+		
+		
+	}//EOR
+	else if((opcode & 0x650) == opcode){
+		printf("EOR ");
+		r_type(instruction, opcode);
+		
+		
+	}//SUB
+	else if((opcode & 0x658) == opcode){
+		printf("SUB ");
+		r_type(instruction, opcode);
+	
+	
+	}//SUBI
+	else if(((opcode >> 1) & 0x344) == (opcode >> 1)){
+		printf("SUBI ");
+		i_type(instruction, opcode);
+		
+		
+	}//EORI
+	else if(((opcode >> 1) & 0x348) == (opcode >> 1)){
+		printf("EORI ");
+		i_type(instruction, opcode);
+		
+		
+	}//LSR
+	else if((opcode & 0x69A) == opcode){
+		printf("LSR ");
+		r_type(instruction, opcode);
+		
+		
+	}//LSL
+	else if((opcode & 0x69B) == opcode){
+		printf("LSL ");
+		r_type(instruction, opcode);
+		
+		
+	}//BR: branch target encoded in RN field
+	else if((opcode & 0x6B0) == opcode){
+		printf("BR ");
+		r_type(instruction, opcode);
+	
+	
+	}//SUBS
+	else if((opcode & 0x758) == opcode){
+		printf("SUBS ");
+		r_type(instruction, opcode);
+		
+		
+	}//SUBIS
+	else if(((opcode >> 1) & 0x3C4) == (opcode >> 1)){
+		printf("SUBIS ");
+		i_type(instruction, opcode);
+		
+		
+	}//STUR
+	else if((opcode & 0x7C0) == opcode){
+		printf("STUR ");
+		d_type(instruction, opcode);
+		
+		
+	}//LDUR
+	else if((opcode & 0x7C2) == opcode){
+		printf("LDUR ");
+		d_type(instruction, opcode);
+		
+		
+	}//PRNL
+	else if((opcode & 0x7FC) == opcode){
+		printf("PRNL\n");
+		
+		
+	}//PRNT
+	else if((opcode & 0x7FD) == opcode){
+		printf("PRNT\n");
+		
+		
+	}//DUMP
+	else if((opcode & 0x7FE) == opcode){
+		printf("DUMP\n");
+		
+		
+	}//HALT
+	else if((opcode & 0x7FF) == opcode){
+		printf("HALT\n");
+		
+		
+	}
+	else{
+		printf("invalid command");
+		continue;
+	}
+	
+	
+	global_i++;
 
 }
